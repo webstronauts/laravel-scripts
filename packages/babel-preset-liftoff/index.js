345c50736921e8015e982ac9d4dec40d269a52f7
Robin van der Vleuten <robin@webstronauts.co>
2018-04-10 16:50:55 +0200

    Moved babel preset to separate package

---
+++ packages/babel-preset-liftoff/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+  presets: [
+    [require.resolve('@babel/preset-env'), {
+      'modules': false
+    }],
+    require.resolve('@babel/preset-react'),
+    require.resolve('@babel/preset-stage-2')
+  ],
+  env: {
+    test: {
+      plugins: [
+        require.resolve('@babel/plugin-transform-modules-commonjs')
+      ]
+    }
+  }
+}
--- packages/laravel-scripts/config/babel.js
+++ packages/laravel-scripts/config/babel.js
@@ -1,17 +1,1 @@
-// @TODO Move this to a separate package
-module.exports = {
-  presets: [
-    [require.resolve('@babel/preset-env'), {
-      'modules': false
-    }],
-    require.resolve('@babel/preset-react'),
-    require.resolve('@babel/preset-stage-2')
-  ],
-  env: {
-    test: {
-      plugins: [
-        require.resolve('@babel/plugin-transform-modules-commonjs')
-      ]
-    }
-  }
-}
+module.exports = require('@webstronauts/babel-preset-liftoff')
