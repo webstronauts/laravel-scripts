92b0237e73d0dcb0f0fa7fa5b12363fb5c2a389e
Robin van der Vleuten <robin@webstronauts.co>
2018-05-21 12:06:18 +0200

    Replaced standard with prettier

--- bin/laravel-scripts.js
+++ bin/laravel-scripts.js
@@ -8,35 +8,44 @@ switch (script) {
   case 'build':
   case 'start':
   case 'test': {
-    const result = spawn.sync('node', [require.resolve('../lib/scripts/' + script)].concat(args), { stdio: 'inherit' })
+    const result = spawn.sync(
+      'node',
+      [require.resolve('../lib/scripts/' + script)].concat(args),
+      {stdio: 'inherit'},
+    )
 
     if (result.signal) {
       if (result.signal === 'SIGKILL') {
-        console.log(`
+        console.log(
+          `
           The build failed because the process exited too early.
           This probably means the system ran out of memory or someone called
           \`kill -9\` on the process.
-        `.trim())
+        `.trim(),
+        )
       } else if (result.signal === 'SIGTERM') {
-        console.log(`
+        console.log(
+          `
           The build failed because the process exited too early.
           Someone might have called \`kill\` or \`killall\`, or the system could
           be shutting down.'
-        `.trim())
+        `.trim(),
+        )
       }
 
       process.exit(1)
     }
 
     process.exit(result.status)
-    // eslint-disable-next-line no-unreachable
     break
   }
 
   default:
-    console.log(`
+    console.log(
+      `
       Unknown script "${script}".
       Perhaps you need to update laravel-scripts?
-    `.trim())
+    `.trim(),
+    )
     break
 }
